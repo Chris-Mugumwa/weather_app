50a3b58afae8012f7b51cc6a913db905e1523354
Ngonidzashe Christian Mugumwa <ngoni.mugumwa@gmail.com>
2021-04-12 13:53:05 +0200

    attempting to fix a stye loading issue

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -1008,7 +1008,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46133" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43095" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
